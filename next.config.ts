2d708c8ad6b790a91b5bca050c56e01bf46e68ce
mascaradee <lamascaradee@gmail.com>
2025-07-14 19:43:18 +0900

    fix: 저장소명 업데이트

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/portfolio-gallery', // 깃허브 저장소 이름
+  basePath: 'https://mignonwhale.github.io/portfolio-gallery/', // 깃허브 저장소 이름
   images: {
     unoptimized: true,
   },
