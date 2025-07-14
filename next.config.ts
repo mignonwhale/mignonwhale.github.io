7e174a0a9ad16e2fe259b1fd0a227533ee814e5b
mascaradee <lamascaradee@gmail.com>
2025-07-14 21:29:17 +0900

    Update

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/portfolio-gallery' : '', // 깃허브 저장소 이름
   // basePath: '/portfolio-gallery', // 깃허브 저장소 이름
   // assetPrefix: '/portfolio-gallery/',
   images: {
